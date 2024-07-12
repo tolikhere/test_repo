4ceaea7b814e352807a851cd741cb1eb48a6f8b3
Tolik <fenixdurangoadrenalin@gmail.com>
2024-07-12 07:31:36 +0300

    Add comments

--- mymodule.py
+++ mymodule.py
@@ -1,5 +1,9 @@
+# Previously I had some complex logic in this
+# module, but I had to delete it due to reasons.
+# Very sad!
+
 def my_function(first, second):
-    return first * second + 10
+    return first + second
 
 def another_function(first):
     return first + 1
